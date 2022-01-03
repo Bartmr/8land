311043b7f05b6e4a855799aea0d3edbb85e5478f
Bartolomeu <pbartmr@gmail.com>
2022-01-03 15:51:08 +0000

    Font fixes

--- client-side/web-app/src/components/ui-kit/fonts-ssr-apis.tsx
+++ client-side/web-app/src/components/ui-kit/fonts-ssr-apis.tsx
@@ -10,7 +10,18 @@ export const attachFontsProviders = ({
   replacePostBodyComponents([
     ...elements,
     <link
-      key="font-stylesheet"
+      key={'fonts-googleapis-preconnect'}
+      rel="preconnect"
+      href="https://fonts.googleapis.com"
+    />,
+    <link
+      key={'fonts-gstatic-preconnect'}
+      rel="preconnect"
+      href="https://fonts.gstatic.com"
+      crossOrigin="anonymous"
+    />,
+    <link
+      key={'font1-stylesheet'}
       href="https://fonts.googleapis.com/css2?family=VT323&display=swap"
       rel="stylesheet"
     />,
