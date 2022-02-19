8d0ccee8cb3d29a71e62e9982d6d33d85bafe311
Bartolomeu <pbartmr@gmail.com>
2022-02-19 18:19:50 +0000

    Commit

--- libs/shared/src/project-details.ts
+++ libs/shared/src/project-details.ts
@@ -1,12 +1,12 @@
 export const PROJECT_VENDOR_NAME = '8Land';
 export const PROJECT_NAME = '8Land';
 export const CONTACT_CTA_PAST =
-  'sent as a direct message on Twitter to <a href="https://twitter.com/8land">@8land</a>';
+  'sent as a direct message on Twitter to <a href="https://twitter.com/explore8land">@explore8land</a>';
 export const CONTACT_CTA_PRESENT =
-  'send us as a direct message on Twitter to <a href="https://twitter.com/8land">@8land</a>';
+  'send us as a direct message on Twitter to <a href="https://twitter.com/explore8land">@explore8land</a>';
 export const CONTACT_CTA_PRESENT_CONTINUOUS =
-  'sending a direct message on Twitter to <a href="https://twitter.com/8land">@8land</a>';
+  'sending a direct message on Twitter to <a href="https://twitter.com/explore8land">@explore8land</a>';
 export const CONTACT_CTA =
-  'Twitter <a href="https://twitter.com/8land">@8land</a>';
+  'Twitter <a href="https://twitter.com/explore8land">@explore8land</a>';
 
 export const PROJECT_SLOGAN = 'The pixelart metaverse';
