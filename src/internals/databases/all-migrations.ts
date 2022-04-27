81db5356730590b1e25381720fa6a0943c5702e5
Bartolomeu <pbartmr@gmail.com>
2022-04-27 08:57:53 +0000

    Commit

--- src/internals/databases/all-migrations.ts
+++ src/internals/databases/all-migrations.ts
@@ -2,10 +2,12 @@ import { FirstMigration1642874083482 } from './migrations/1642874083482-FirstMig
 import { NFTMetadata1645870824982 } from './migrations/1645870824982-NFTMetadata';
 import { UserAppId1647382874239 } from './migrations/1647382874239-UserAppId';
 import { NavigationState1649620686884 } from './migrations/1649620686884-NavigationState';
+import { Train1651049820141 } from './migrations/1651049820141-Train';
 
 export const ALL_MIGRATIONS = [
   FirstMigration1642874083482,
   NFTMetadata1645870824982,
   UserAppId1647382874239,
   NavigationState1649620686884,
+  Train1651049820141,
 ];
--- src/users/typeorm/train-state.entity.ts
+++ src/users/typeorm/train-state.entity.ts
@@ -1,8 +1,9 @@
 import { SimpleEntity } from 'src/internals/databases/simple-entity/simple.entity';
 import { Land } from 'src/land/typeorm/land.entity';
-import { CreateDateColumn, ManyToOne, UpdateDateColumn } from 'typeorm';
+import { CreateDateColumn, Entity, ManyToOne, UpdateDateColumn } from 'typeorm';
 import { User } from './user.entity';
 
+@Entity()
 export class TrainState extends SimpleEntity {
   @CreateDateColumn()
   createdAt!: Date;
