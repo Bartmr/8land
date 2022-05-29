6e22d047b035d608bc11fdda8c5c368ad894ecf0
Bartolomeu <pbartmr@gmail.com>
2022-05-29 15:22:44 +0000

    Commit

--- src/train/train.controller.ts
+++ src/train/train.controller.ts
@@ -15,6 +15,7 @@ import { AuditContext } from 'src/internals/auditing/audit-context';
 import { WithAuditContext } from 'src/internals/auditing/audit.decorator';
 import { ResourceNotFoundException } from 'src/internals/server/resource-not-found.exception';
 import { LandsService } from 'src/land/lands.service';
+import { Land } from 'src/land/typeorm/land.entity';
 import { LandRepository } from 'src/land/typeorm/land.repository';
 import { NavigationStateRepository } from 'src/users/typeorm/navigation-state.repository';
 import { Connection } from 'typeorm';
@@ -26,60 +27,77 @@ export class TrainController {
     private landsService: LandsService,
   ) {}
 
-  // @Get('/world/:worldId')
-  // async board(
-  //   @Param() param: BoardTrainParametersDTO,
-  //   @WithAuditContext() auditContext: AuditContext,
-  //   @WithOptionalAuthContext() authContext?: AuthContext,
-  // ): Promise<BoardTrainDTO> {
-  //   return this.connection.transaction(async (eM) => {
-  //     const landsRepository = eM.getCustomRepository(LandRepository);
-  //     const navStatesRepository = eM.getCustomRepository(
-  //       NavigationStateRepository,
-  //     );
+  @Get('/world/:worldId')
+  async board(
+    @Param() param: BoardTrainParametersDTO,
+    @WithAuditContext() auditContext: AuditContext,
+    @WithOptionalAuthContext() authContext?: AuthContext,
+  ): Promise<BoardTrainDTO> {
+    return this.connection.transaction(async (eM) => {
+      const landsRepository = eM.getCustomRepository(LandRepository);
+      const navStatesRepository = eM.getCustomRepository(
+        NavigationStateRepository,
+      );
 
-  //     const land = await landsRepository.selectOne(
-  //       {
-  //         alias: 'land',
-  //       },
+      const land = await landsRepository.selectOne(
+        {
+          alias: 'land',
+        },
 
-  //       (qb) =>
-  //         qb
-  //           .orderBy('land.createdAt')
-  //           .leftJoinAndSelect('land.world', 'world')
-  //           .where('world.user = :id', { id: param.worldId }),
-  //     );
+        (qb) =>
+          qb
+            .orderBy('land.createdAt')
+            .leftJoinAndSelect('land.world', 'world')
+            .where('world.user = :id', { id: param.worldId }),
+      );
 
-  //     if (!land) {
-  //       throw new ResourceNotFoundException();
-  //     }
+      if (!land) {
+        throw new ResourceNotFoundException();
+      }
 
-  //     if (!authContext) {
-  //       return this.landsService.mapLand(land);
-  //     } else {
-  //       const navState = await navStatesRepository.getNavigationStateFromUser(
-  //         authContext.user,
-  //         { auditContext, eM },
-  //       );
+      if (!authContext) {
+        return this.landsService.mapLand(land);
+      } else {
+        const navState = await navStatesRepository.getNavigationStateFromUser(
+          authContext.user,
+          { auditContext, eM },
+        );
 
-  //       if (navState.lastDoor) {
-  //         if (navState.cameBack && navState.lastDoor.inLand) {
-  //           if (!navState.lastDoor.inLand.isTrainStation) {
-  //             throw new ConflictException({
-  //               error: 'not-boarding-from-a-train-station',
-  //             });
-  //           }
-  //         } else if (navState.lastDoor.toLand) {
-  //           if (!navState.lastDoor.toLand.isTrainStation) {
-  //             throw new ConflictException({
-  //               error: 'not-boarding-from-a-train-station',
-  //             });
-  //           }
-  //         } else {
-  //           throw new NotImplementedException();
-  //         }
-  //       }
-  //     }
-  //   });
-  // }
+        if (!navState.lastDoor) {
+          throw new ConflictException({
+            error: 'not-boarding-from-a-train-station',
+          });
+        }
+
+        let trainStation: Land;
+
+        if (!navState.lastDoor.inLand) {
+          throw new NotImplementedException();
+        }
+
+        if (navState.cameBack) {
+          trainStation = navState.lastDoor.inLand;
+        } else {
+          trainStation = navState.lastDoor.toLand;
+        }
+
+        if (!trainStation.isTrainStation) {
+          throw new ConflictException({
+            error: 'not-boarding-from-a-train-station',
+          });
+        }
+
+        navState.lastDoor = null;
+        navState.cameBack = null;
+        navState.lastPlayedBackgroundMusicUrl = null;
+
+        navState.traveledByTrainToLand = land;
+        navState.boardedOnTrainStation = trainStation;
+
+        await navStatesRepository.save(navState, auditContext);
+
+        return this.landsService.mapLand(land);
+      }
+    });
+  }
 }
