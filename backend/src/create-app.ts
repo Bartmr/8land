16c333bdc555056634880c7e0f80570b47032c03
Bartolomeu Rodrigues <pbartmr@gmail.com>
2026-03-26 18:39:37 +0000

    commit

--- backend/src/create-app.ts
+++ backend/src/create-app.ts
@@ -1,11 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { LoggingService } from './logging/logging.service';
-import { LoggerService } from '@nestjs/common';
-import { NODE_ENV } from './environment/node-env.constants';
-import { NodeEnv } from './environment/node-env.types';
 import { EnvironmentVariablesService } from './environment/environment-variables.service';
-import { PROJECT_NAME } from '@shared/src/project-details';
 import cookieParser from 'cookie-parser';
 
 const WEB_APP_ORIGIN = EnvironmentVariablesService.variables.WEB_APP_ORIGIN;
@@ -19,31 +14,6 @@ export async function createApp() {
     origin: WEB_APP_ORIGIN,
     credentials: true,
   });
-
-  const loggingService = app.get(LoggingService);
-
-  const logger: LoggerService = {
-    log(message: string) {
-      loggingService.logInfo('nestjs-logger-log', message);
-    },
-    error(message: string, trace: string) {
-      loggingService.logError('nestjs-logger-error', new Error(), {
-        message,
-        trace,
-      });
-    },
-    warn(message: string) {
-      loggingService.logWarning('nestjs-logger-warn', message);
-    },
-    debug(message: string) {
-      loggingService.logDebug('nestjs-logger-debug', message);
-    },
-    verbose(message: string) {
-      loggingService.logInfo('nestjs-logger-verbose', message);
-    },
-  };
-
-  app.useLogger(logger);
 
   return app;
 }
