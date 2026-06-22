a90d37f51828c10657cbda08e513198ba1dd416b
Bartolomeu Rodrigues <pbartmr@gmail.com>
2026-06-22 14:55:54 +0100

    commit

--- web-app/src/app.tsx
+++ web-app/src/app.tsx
@@ -1,7 +1,6 @@
 import './logging/logger';
 import './environment-variables';
 import './ui/index.scss';
-import './ui/icons.scss';
 
 import React, { ReactNode } from 'react';
 import { Provider } from 'react-redux';
