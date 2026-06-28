5f1c81421392c16e52f09d8868a99a17d087206f
Bartolomeu Rodrigues <pbartmr@gmail.com>
2026-06-29 00:05:39 +0100

    commit

--- web-app/src/forms/uncompleted-form-types.ts
+++
@@ -1,7 +0,0 @@
-export type UncompletedFormValue<T> = T extends undefined | null
-  ? T
-  : T extends Array<unknown>
-  ? Array<UncompletedFormValue<T[number]> | undefined>
-  : T extends { [key: string]: unknown }
-  ? { [K in keyof T]?: UncompletedFormValue<T[K]> }
-  : T | undefined | null | string;
